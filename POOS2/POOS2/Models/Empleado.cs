839dc06e2ebecbbeda1aaef90e72a3fad8121703
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-04-25 20:32:02 -0600

    Commit de POO Ejercicio ISR

---
+++ POOS2/POOS2/Models/Empleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOS2.Models
+{
+    public class Empleado
+    {
+        // Encapsular: Es el proceso de definicion de caracteristicas y asegurar 
+        // que estas no puedan ser modificadas sin permiso.
+        private string nombre;
+        private double salario;
+
+        // Metodo Contructor (Este metodo nos sirve para inicializar o instanciar los objetos en un programa)
+
+        public Empleado( string nm, double sl ) {
+
+            nombre = nm;
+            salario = sl;
+
+        }
+
+        public string getNombre() {
+            return nombre;
+        }
+
+        public double getSalario()
+        {
+            return salario;
+        }
+
+        public double getISR() {
+
+            return salario*0.15;
+        
+        }
+
+    }
+}
---
+++ POOS2/POOS2/Program.cs
@@ -0,0 +1,29 @@
+﻿using POOS2.Models;
+
+bool salirCiclo = false;
+
+do
+{
+    Console.WriteLine("Ingrese el Nombre del Empleado");
+    string nombre = Console.ReadLine();
+
+    Console.WriteLine("Ingrese el Salario del Empleado");
+    double salario = double.Parse (Console.ReadLine());
+
+    Empleado x = new Empleado(nombre, salario);
+
+    Console.WriteLine($"El ISR del Empleado {x.getNombre()} es de {x.getISR()}");
+
+
+    Console.WriteLine("Quiere Salir del Programa (Y/N)");
+    char salir = char.Parse (Console.ReadLine());
+
+    if (salir == 'Y')
+    {
+
+        salirCiclo = true;
+
+    }
+
+
+} while (salirCiclo == false);
